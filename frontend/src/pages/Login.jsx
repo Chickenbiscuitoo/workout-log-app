cdfd880410e9ea577a2f433e12ce7b1bbe47e202
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-08-09 13:56:26 +0200

    added login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,20 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+
 import styles from '../styles/form.module.css'
 
 function Login() {
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	})
+
+	const { email, password } = formData
+
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
+		(state) => state.auth
+	)
+
+	useEffect(() => {
+		if (isError) {
+			console.log(message)
+		}
+
+		if (isSuccess || user) {
+			navigate('/home')
+		}
+
+		dispatch(reset())
+	}, [user, isError, isSuccess, message, navigate, dispatch])
+
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}))
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+
+		const userData = {
+			email,
+			password,
+		}
+
+		dispatch(login(userData))
+	}
+
+	if (isLoading) {
+		return <Spinner />
+	}
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
 				<h1 className={styles.title}>Sign In</h1>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={onSubmit}>
 					<div className={styles.formgroup}>
-						<input type="text" placeholder="Email" />
+						<input
+							type="text"
+							placeholder="Email"
+							name="email"
+							value={email}
+							onChange={onChange}
+						/>
 					</div>
 					<div className={styles.formgroup}>
-						<input type="text" placeholder="Password" />
+						<input
+							type="text"
+							placeholder="Password"
+							name="password"
+							value={password}
+							onChange={onChange}
+						/>
 					</div>
+					<button className={styles.btn} type="submit">
+						Log In
+					</button>
 				</form>
-				<button className={styles.btn}>Log In</button>
 			</div>
 			<div className={styles.imgcontainer}>
 				<img
